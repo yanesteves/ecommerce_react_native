06599c9c8d5f5230ddd71262ad78580ee9c4ef19
˜Yan <yan.m.esteves@gmail.com>
2024-10-09 20:36:04 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { SafeAreaView, StyleSheet, StatusBar, TouchableOpacity, Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { HomePage } from './src/pages/Home/Home.jsx';
+import { CartPage } from './src/pages/Cart/Cart.jsx';
+import Icon from 'react-native-vector-icons/Ionicons'; // Importando o ícone do carrinho
 
-export default function App() {
+import './gesture-handler.native';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.safeArea} edges={['top', 'left', 'right']}>
+      <NavigationContainer>
+        <StatusBar
+          backgroundColor="transparent"
+          translucent={true}
+          barStyle="dark-content" // Define o estilo do conteúdo da StatusBar (claro ou escuro)
+        />
+
+        <Drawer.Navigator initialRouteName="Products">
+          <Drawer.Screen 
+            name="E-Commerce" 
+            component={HomePage} 
+            options={{
+              headerRight: () => <CartIconWithNavigation />,
+              title: 'E-Commerce',
+            }}/>
+          <Drawer.Screen name="Cart" component={CartPage} />
+        </Drawer.Navigator>
+
+      </NavigationContainer>
+    </SafeAreaView>
+
+  )
 }
 
+
+const CartIconWithNavigation = () => {
+  const navigation = useNavigation();
+  const [cartItemCount, setCartItemCount] = useState(3); // Simulação de quantidade de itens no carrinho
+
+  return (
+    <TouchableOpacity
+      style={styles.cartContainer}
+      onPress={() => navigation.navigate('Cart')}
+    >
+      <Icon name="cart-outline" size={24} color="#000" />
+      <Text style={styles.cartCount}>{cartItemCount}</Text>
+    </TouchableOpacity>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  cartContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginRight: 16,
+  },
+  cartCount: {
+    marginLeft: 4,
+    fontSize: 16,
+    color: '#000',
+    fontWeight: 'bold',
   },
 });
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export const HomePage = () => {
+    return (
+        <View>
+            <Text>Home Page</Text>
+        </View>
+    )
+}
